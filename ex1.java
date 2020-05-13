768022643d302a3a54f0d492429988db066bab0c
unknown <mateus.sa94@hotmail.com>
2020-05-13 20:24:00 -0300

    arquivo java

---
+++ ex1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+		public class ex1{
+
+		public  static void main (String [] args){
+		Scanner entrada=new Scanner (System.in);
+		System.out.println("Nome do usuario: ");
+		
+		String nome = entrada.nextLine();
+		System.out.println("Ola"+nome+"seja bem-vindo!");
+	}
+}
